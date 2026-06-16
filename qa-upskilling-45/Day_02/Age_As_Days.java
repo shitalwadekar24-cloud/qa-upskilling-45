a04dab8175e83ea83bc2c71ad2a906bd010c126f
Shital Wadekar <shitalwadekar24@gmail.com>
2026-06-16 11:10:08 +0530

    Completed Day 2 - Variables, Data Types and Operators

--- qa-upskilling-45/Day_02/Age_As_Days.java
+++ qa-upskilling-45/Day_02/Age_As_Days.java
@@ -4,7 +4,7 @@ public class Age_As_Days {
 	public static void main(String[] args) {
        int age= 33;
        int days= age*365;
-       System.out.println("Days as Age:"+days);
+       System.out.println("Days as Age: test"+days);
 	}
 
 }
